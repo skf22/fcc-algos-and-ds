d9649fe7b0ca90c982319c730e47ba9b1e5a33a8
Shirjeel Khalil Fahid <skf11987@gmail.com>
2021-06-30 21:12:15 +0100

    036 - Word Blanks

---
+++ Basic Javascript/036 - Word Blanks/index.js
@@ -0,0 +1,7 @@
+var myNoun = "dog";
+var myAdjective = "big";
+var myVerb = "ran";
+var myAdverb = "quickly";
+
+var wordBlanks =
+  "The " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + ".";
