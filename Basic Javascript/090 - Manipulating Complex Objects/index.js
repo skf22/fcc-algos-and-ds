1f4711f7cf25f51052611529c12dbfa9ae458759
Shirjeel Khalil Fahid <skf11987@gmail.com>
2022-03-22 14:12:00 +0000

    090 - Manipulating Complex Objects

---
+++ Basic Javascript/090 - Manipulating Complex Objects/index.js
@@ -0,0 +1,16 @@
+const myMusic = [
+  {
+    artist: "Billy Joel",
+    title: "Piano Man",
+    release_year: 1973,
+    formats: ["CD", "8T", "LP"],
+    gold: true,
+  },
+  {
+    artist: "Lio",
+    title: "Suite Sixteen",
+    release_year: 1981,
+    formats: ["CD", "8T", "LP"],
+    gold: false,
+  },
+];
