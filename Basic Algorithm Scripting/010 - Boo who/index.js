609f29bbd726b0f6c5f4c96b014fcbfd5f7c285d
Shirjeel Khalil Fahid <skf11987@gmail.com>
2022-03-26 14:38:49 +0000

    010 - Boo who

--- Basic Algorithm Scripting/010 - Boo who/index.js
+++ Basic Algorithm Scripting/010 - Boo who/index.js
@@ -1,3 +1,9 @@
 const booWho = (bool) => (bool === true || bool === false ? true : false);
 
 booWho(null);
+
+// Alternative Solution - N.B. The below would not pass a test for booWho(null) (which is not one of the tests in freeCodeCamp despite being in the example code) because in Javascript, null is classed as an object, and therefore typeof(null) would not return a boolean, rather it would return "object". Nevertheless, it is an acceptable solution to the problem outside of this edge-case.
+
+// const booWho = (bool) => typeof bool === "boolean";
+
+// booWho(null);
