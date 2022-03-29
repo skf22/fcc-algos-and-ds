ace7c5e4ad279b74775e74324a4c497344cb04bd
Shirjeel Khalil Fahid <skf11987@gmail.com>
2022-03-29 16:07:27 +0100

    001 - Sum All Numbers in a Range

---
+++ Intermediate Algorithm Scripting/001 - Sum All Numbers in a Range/index.js
@@ -0,0 +1,10 @@
+const sumAll = (arr) => {
+  arr.sort((a, b) => a - b);
+  let sum = 0;
+  for (let i = arr[0]; i <= arr[arr.length - 1]; i++) {
+    sum += i;
+  }
+  return sum;
+};
+
+sumAll([4, 1]);
