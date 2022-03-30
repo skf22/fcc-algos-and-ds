636b6011b4f780156d36b315600da64afc3767fe
Shirjeel Khalil Fahid <skf11987@gmail.com>
2022-03-30 17:32:18 +0100

    009 - Missing Letters

---
+++ Intermediate Algorithm Scripting/009 - Missing Letters/index.js
@@ -0,0 +1,10 @@
+const fearNotLetter = (str) => {
+  for (let i = 0; i < str.length; i++) {
+    if (str.charCodeAt(i) !== str.charCodeAt(0) + i) {
+      return String.fromCharCode(str.charCodeAt(i) - 1);
+    }
+  }
+  return undefined;
+};
+
+fearNotLetter("abce");
---
+++ Intermediate Algorithm Scripting/009 - Missing Letters/missing-letters.md
@@ -0,0 +1,7 @@
+### **Missing letters**
+
+<br>
+
+Find the missing letter in the passed letter range and return it.
+
+If all letters are present in the range, return `undefined`.
